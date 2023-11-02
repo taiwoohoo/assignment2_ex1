d36598f9f75bcc9e6fa250b5ed2ad8a955100d41
TCH <chunhung@TCHdeMacBook-Pro.local>
2023-11-02 12:21:12 +0000

    Added new functions to rule out cars with over 200 hp and arrangge the order by hp

--- exercise1.R
+++ exercise1.R
@@ -2,10 +2,12 @@
 
 library(tidyverse)
 
-#display the car with over 100 horsepower
+#display the cars with over 100 and below 200 horsepower and arrange the order by horsepower 
 mtcars_hp_high <- function(data){
   mtcars_1 <- data %>% 
-    filter(hp>100)
+    filter(hp>100) %>% #filter for cars with over 100 horsepower
+    filter(hp<200) %>% #filter for cars below 200 horsepower
+    arrange(hp) #arrange cars by horsepower
   return(mtcars_1)
 }
 
